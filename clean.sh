c96150d9d62db883ab5697b495d80c4358459e27
Ben <ben.bridts@gmail.com>
2023-04-23 16:04:34 +0200

    clean more

--- clean.sh
+++ clean.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 set -ex
-
+echo "Current disk usage"
+df -h
+echo "Starting clean"
 docker system prune
 brew cleanup -s --prune=all
 go clean -cache -modcache
 npm cache clean --force
 yarn cache clean --force
 
+# remove almost all pyenvs
 for v in $(pyenv versions | grep 3. | sort -Vr | tail -n+2); do
     pyenv uninstall -f $v
 done
 # vagrant box prune
 # vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant box remove -f
+
+# remove pipenv caches
 rm -rf ~/Library/Caches/pipenv/*
+# remove pipenv (central) virtualenvs
 rm -rf ~/.local/share/virtualenvs/*
+# remove poetry *central) virtualenvs and caches
+rm -rf ~/ben/Library/Caches/pypoetry/*
 rm -rf ~/.cache/*
 rm -rf ~/.gradle/caches
 
 find ~/src -type d -path '*/.aws-sam/build' -print -exec rm -rf \{\} +
 find ~/src -type d -name '.venv' -print -exec rm -rf \{\} +
+find ~/src -type d -name 'venv' -print -exec rm -rf \{\} +
 
 echo "done"
+echo "Current disk usage"
+df -h
